fb1daee80bf167766621c82979378e8056d28ae3
Daniel Kaptejny <danielk9388@gmail.com>
2019-11-12 21:46:56 +0100

    string resources to PCl

--- Debtors.Core/Interfaces/ILogService.cs
+++ Debtors.Core/Interfaces/ILogService.cs
@@ -11,5 +11,19 @@ namespace Debtors.Core.Interfaces
 		void Error(Exception ex, string message);
 		void Error(string format, params object[] args);
 		void Error(Exception ex, string format, params object[] args);
+
+		void Fatal(string message);
+		void Fatal(string format, params object[] args);
+		void Fatal(Exception e, string message);
+		void Fatal(Exception e, string format, params object[] args);
+
+		void Debug(string message);
+		void Debug(string format, params object[] args);
+
+		void Trace(string message);
+		void Trace(string format, params object[] args);
+
+		void Warn(string message);
+		void Warn(string format, params object[] args);
 	}
 }
--- Debtors.Core/Services/LogService.cs
+++ Debtors.Core/Services/LogService.cs
@@ -21,10 +21,40 @@ namespace Debtors.Core.Services
 			logger = LogManager.GetLogger("NLogSample");
 		}
 
+		#region Error
 		public void Error(string message) => logger.Error(message);
 		public void Error(Exception ex) => logger.Error(ex, ex.Message);
-		public void Error(Exception ex, string message) => logger.Error(ex, message);
+		public void Error(Exception e, string message) => logger.Error(e, message);
 		public void Error(string format, params object[] args) => logger.Error(format, args);
-		public void Error(Exception ex, string format, params object[] args) => logger.Error(ex, format, args);
+		public void Error(Exception e, string format, params object[] args) => logger.Error(e, format, args);
+		#endregion
+
+		#region Fatal
+		public void Fatal(string message) => logger.Fatal(message);
+		public void Fatal(string format, params object[] args) => logger.Fatal(format, args);
+
+		public void Fatal(Exception e, string message) => logger.Fatal(e, message);
+		public void Fatal(Exception e, string format, params object[] args) => logger.Fatal(e, format, args);
+		#endregion
+
+		#region Debug
+		public void Debug(string message) => logger.Debug(message);
+		public void Debug(string format, params object[] args) => logger.Debug(format, args);
+		#endregion
+
+		#region Info
+		public void Info(string message) => logger.Info(message);
+		public void Info(string message, params object[] args) => logger.Info(message, args);
+		#endregion
+
+		#region Trace
+		public void Trace(string message) => logger.Trace(message);
+		public void Trace(string format, params object[] args) => logger.Trace(format, args);
+		#endregion
+
+		#region Warn
+		public void Warn(string message) => logger.Warn(message);
+		public void Warn(string format, params object[] args) => logger.Warn(format, args);
+		#endregion
 	}
 }
